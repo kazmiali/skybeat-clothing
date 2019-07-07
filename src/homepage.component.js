4db3b3ac875618f3e99e64d44ef9cc5bc65dcb6e
Kaaazmi <39811688+Kaaazmi@users.noreply.github.com>
2019-07-07 23:35:50 +0500

    Initialized the project and added the styling

---
+++ src/homepage.component.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const HomePage = () => {
+	return (
+		<div className='homepage'>
+			<div className='directory-menu'>
+				<div className='menu-item'>
+					<div className='content'>
+						<h1 className='title'>Jackets</h1>
+						<span className='substitute'>Shop Now !</span>
+					</div>
+				</div>
+				<div className='menu-item'>
+					<div className='content'>
+						<h1 className='title'>Shirts</h1>
+						<span className='substitute'>Shop Now !</span>
+					</div>
+				</div>
+				<div className='menu-item'>
+					<div className='content'>
+						<h1 className='title'>Men's</h1>
+						<span className='substitute'>Shop Now !</span>
+					</div>
+				</div>
+				<div className='menu-item'>
+					<div className='content'>
+						<h1 className='title'>Women's</h1>
+						<span className='substitute'>Shop Now !</span>
+					</div>
+				</div>
+				<div className='menu-item'>
+					<div className='content'>
+						<h1 className='title'>Suits</h1>
+						<span className='substitute'>Shop Now !</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default HomePage;
