f70b2cd8561dfb799f7bcda48296b823432b6d78
Muhammad Ali Kazmi <ali.smak099@gmail.com>
2019-08-17 07:24:35 +0500

    added stripe integration and added favicon icon and changed the title

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -5,6 +5,7 @@ import { createStructuredSelector } from 'reselect';
 import './checkout.styles.scss';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import {
 	selectCartItems,
@@ -37,6 +38,11 @@ const CheckoutPage = ({ cartItems, total }) => (
 		<div className='total'>
 			<span>{`TOTAL: $${total}`}</span>
 		</div>
+		<div className='test-warning'>
+			*Please use the test credit card for payment <br />
+			4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+		</div>
+		<StripeCheckoutButton price={total} />
 	</div>
 );
 
