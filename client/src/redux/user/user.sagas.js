ee83839f2b3af96f0b3e87ef35377333dd26c12a
Kaaazmi <39811688+Kaaazmi@users.noreply.github.com>
2019-10-15 00:30:49 +0500

    :rocket: deploying stuff

--- client/src/redux/user/user.sagas.js
+++ client/src/redux/user/user.sagas.js
@@ -49,7 +49,7 @@ export function* signInWithEmail({ payload: { email, password } }) {
 export function* isUserAuthenticated() {
 	try {
 		const userAuth = yield getCurrentUser();
-		// if (!userAuth) return;
+		if (!userAuth) return;
 		yield getSnapshotFromUserAuth(userAuth);
 	} catch (error) {
 		yield put(signInFailure(error));
