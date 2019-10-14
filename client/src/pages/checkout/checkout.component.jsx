3a16d8063b1294238f9b822b3228f75af1c6bc0a
Muhammad Ali Kazmi <ali.smak099@gmail.com>
2019-10-15 02:07:58 +0500

    :zap: Add yarn as engine and added condition to render pay now

--- client/src/pages/checkout/checkout.component.jsx
+++ client/src/pages/checkout/checkout.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
@@ -41,12 +41,16 @@ const CheckoutPage = ({ cartItems, total }) => (
 			<CheckoutItem key={cartItem.id} cartItem={cartItem} />
 		))}
 		<TotalContainer>TOTAL: ${total}</TotalContainer>
-		<WarningContainer>
-			*Please use the following test credit card for payments*
-			<br />
-			4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
-		</WarningContainer>
-		<StripeCheckoutButton price={total} />
+		{total !== 0 ? (
+			<Fragment>
+				<WarningContainer>
+					*Please use the following test credit card for payments*
+					<br />
+					4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+				</WarningContainer>
+				<StripeCheckoutButton price={total} />
+			</Fragment>
+		) : null}
 	</CheckoutPageContainer>
 );
 
