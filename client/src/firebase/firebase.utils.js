890a2be44b04111f9cb0ed63f57a4a318cfdf0fc
Muhammad Ali Kazmi <ali.smak099@gmail.com>
2019-11-10 02:25:05 +0500

    :fire: Hide API KEY

--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -3,7 +3,7 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-	apiKey: 'AIzaSyB6tkw4dEZ_0ptrw_A-Yj2SUC8_48-qUoA',
+	apiKey: FIREBASE_API_KEY,
 	authDomain: 'crwn-db-1049.firebaseapp.com',
 	databaseURL: 'https://crwn-db-1049.firebaseio.com',
 	projectId: 'crwn-db-1049',
