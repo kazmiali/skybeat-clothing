cd100c478d3a94ccaf9dc13a22b5d811f591074b
Muhammad Ali Kazmi <ali.smak099@gmail.com>
2020-01-28 11:06:42 +0500

    :zap: Edit styles

--- client/src/components/header/header.component.jsx
+++ client/src/components/header/header.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { ReactComponent as Logo } from '../../assets/retail.svg';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from './../cart-dropdown/cart-dropdown';
 
@@ -11,43 +11,40 @@ import { selectCurrentUser } from '../../redux/user/user.selectors';
 import { signOutStart } from '../../redux/user/user.actions';
 
 import {
-	HeaderContainer,
-	OptionsContainer,
-	OptionLink,
-	LogoContainer
+  HeaderContainer,
+  OptionsContainer,
+  OptionLink,
+  LogoContainer,
 } from './header.styles';
 
 const Header = ({ currentUser, hidden, signOutStart }) => (
-	<HeaderContainer>
-		<LogoContainer to='/'>
-			<Logo />
-		</LogoContainer>
-		<OptionsContainer>
-			<OptionLink to='/shop'>SHOP</OptionLink>
-			<OptionLink to='/shop'>CONTACT</OptionLink>
-			{currentUser ? (
-				<OptionLink as='div' onClick={signOutStart}>
-					SIGN OUT
-				</OptionLink>
-			) : (
-				<OptionLink to='/signin'>SIGN IN</OptionLink>
-			)}
-			<CartIcon />
-		</OptionsContainer>
-		{hidden ? <CartDropdown /> : null}
-	</HeaderContainer>
+  <HeaderContainer>
+    <LogoContainer to='/'>
+      <Logo style={{ maxHeight: '100%', maxWidth: '100%' }} />
+    </LogoContainer>
+    <OptionsContainer>
+      <OptionLink to='/shop'>SHOP</OptionLink>
+      <OptionLink to='/shop'>CONTACT</OptionLink>
+      {currentUser ? (
+        <OptionLink as='div' onClick={signOutStart}>
+          SIGN OUT
+        </OptionLink>
+      ) : (
+        <OptionLink to='/signin'>SIGN IN</OptionLink>
+      )}
+      <CartIcon />
+    </OptionsContainer>
+    {hidden ? <CartDropdown /> : null}
+  </HeaderContainer>
 );
 
 const mapDispatchToProps = dispatch => ({
-	signOutStart: () => dispatch(signOutStart())
+  signOutStart: () => dispatch(signOutStart()),
 });
 
 const mapStateToProps = createStructuredSelector({
-	currentUser: selectCurrentUser,
-	hidden: selectHidden
+  currentUser: selectCurrentUser,
+  hidden: selectHidden,
 });
 
-export default connect(
-	mapStateToProps,
-	mapDispatchToProps
-)(Header);
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
--- client/src/components/header/header.styles.jsx
+++ client/src/components/header/header.styles.jsx
@@ -2,46 +2,57 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 export const HeaderContainer = styled.div`
-	height: 70px;
-	width: 100%;
-	display: flex;
-	justify-content: space-between;
-	margin-bottom: 25px;
+  height: 70px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 25px;
 
-	@media screen and (max-width: 800px) {
-		height: 60px;
-		padding: 10px;
-		margin-bottom: 20px;
-	}
+  @media screen and (max-width: 800px) {
+    height: 60px;
+    padding: 10px;
+    margin-bottom: 20px;
+  }
 `;
 
 export const LogoContainer = styled(Link)`
-	height: 100%;
-	width: 70px;
-	padding: 25px;
+  height: 70%;
+  width: 70px;
+  margin-left: 0.2rem;
+  margin-top: 0.6rem;
+  padding: 0px 0 0 0;
 
-	@media screen and (max-width: 800px) {
-		width: 50px;
-		padding 0px;
+  @media screen and (max-width: 800px) {
+    width: 50px;
+    height: 40px;
+    padding: 0px;
+    margin: 0;
   }
 `;
 
 export const OptionsContainer = styled.div`
-	width: 50%;
-	height: 100%;
-	display: flex;
-	align-items: center;
-	justify-content: flex-end;
+  width: 50%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
 
-	@media screen and (max-width: 800px) {
-		width: 80%;
-	}
+  @media screen and (max-width: 800px) {
+    width: 80%;
+  }
 `;
 
 export const OptionLink = styled(Link)`
-	padding: 10px 15px;
-	text-decoration: none;
-	color: #000004;
-	font-weight: 900;
-	cursor: pointer;
+  padding: 6px 12px;
+  text-decoration: none;
+  font-size: 1.225rem;
+  color: #000004;
+  font-weight: 900;
+  cursor: pointer;
+  letter-spacing: 2px;
+
+  @media screen and (max-width: 800px) {
+    font-size: 1rem;
+    padding: 10px 12px;
+  }
 `;
