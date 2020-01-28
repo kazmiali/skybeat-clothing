2b4286c24ca7b6aac4b89cda216d0e999c261fbe
Muhammad Ali Kazmi <ali.smak099@gmail.com>
2020-01-28 14:37:32 +0500

    :zap: Styled component

--- client/src/components/header/header.styles.jsx
+++ client/src/components/header/header.styles.jsx
@@ -40,9 +40,11 @@ export const OptionsContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
+  margin: 0 1rem 0 0;
 
   @media screen and (max-width: 800px) {
     width: 80%;
+    margin: 0px;
   }
 `;
 
